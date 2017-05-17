47a6b58abcf5ec6ee1d0c1fd21e1a28e2204f144
Hack Reactor Student <student@hackreactor.com>
2017-05-17 15:17:04 -0700

    done with queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,33 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var size = 0;
   someInstance.enqueue = function(value) {
+    storage[size] = value;
+    size++;
   };
+  
+  var resetKey = function () {
+    for (let key in storage) {
+      storage[key - 1] = storage[key];
+    }
+  };
+  
 
   someInstance.dequeue = function() {
+    //function resetKey()
+    var temp = storage[0];
+    delete storage[0];
+    size--; 
+    resetKey();
+    return temp;
   };
 
   someInstance.size = function() {
+    if (size < 0) {
+      return 0;
+    }
+    return size; 
   };
 
   return someInstance;
