cd887573e7320d242ac04c7a526db09a43bfafe6
Hack Reactor Student <student@hackreactor.com>
2017-05-17 20:01:38 -0700

    completed stacks and queues

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,31 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(val) {
+  this.storage[this.count] = val;
+  this.count++;
+};
+
+Queue.prototype.reList = function() {
+  for ( let key in this.storage) {
+    this.storage[key - 1] = this.storage[key];
+  }
+  delete this.storage[this.count];
+};
+Queue.prototype.dequeue = function() {
+  let temp = this.storage[0];
+  this.count--;
+  delete this.storage[0];
+  this.reList();
+  return temp;
+};
+
+Queue.prototype.size = function() {
+  if ( this.count < 0) {
+    return 0;
+  }
+  return this.count;
+};
 
