eb3fb6aee9de7c4feee58a540c5a83e266783526
Hack Reactor Student <student@hackreactor.com>
2017-05-19 18:28:50 -0700

    style graph

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,11 @@
 var BinarySearchTree = function(value) {
+  
+
 };
 
+// BinarySearchTree.prototype.
+//   // body...
+// };
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,10 +1,4 @@
-// var obj = {
-//   1: [2, 3, 4],
-//   2: [1, 4, 5],
-//   3: [23, 23]
-// };
 
-// Instantiate a new graph
 var Graph = function() {
   this.storage = {};
 };
@@ -12,8 +6,6 @@ var Graph = function() {
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
   this.storage[node] = [];
-  console.log(this.storage);
-  // console.log(this.storage);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -23,39 +15,16 @@ Graph.prototype.contains = function(node) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  // console.log('this is the node we want to remove', node);
   delete this.storage[node];
-  // console.log(this.storage[node]);
-  //iterate through this.storage
-    //run removeEdge(this.storage[element], node)
-
   for (let key in this.storage) {
-    // console.log(this.storage[key]);
     this.removeEdge(key, node);
-    // console.log(this.storage[key]);
-
   }
-
-
-
-  
-
-  // _.each(this.storage, element => {
-  //   this.removeEdge(element, node);
-  // });
-   
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  return _.contains(this.storage[fromNode], toNode);
-  // _.each(this.storage[fromNode], element => {
-  //   if (element === toNode) {
-  //     return true;
-  //   }
-  // });
-
-  // return false;
+  return _.contains(this.storage[fromNode], toNode ) 
+      && _.contains(this.storage[toNode], fromNode);
   
 };
 
@@ -88,24 +57,10 @@ Graph.prototype.removeEdge = function(fromNode, toNode) {
 Graph.prototype.forEachNode = function(cb) {
 
   for (let key in this.storage) {
-    cb(key);
-
+    cb(parseInt(key));
   }
-  // _.each(this.storage, element => {
-  //   _.each(element, el => {
-  //     cb (el);
-  //   });
-  // });
-
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-var g = new Graph()
-g.addNode(1);
-g.addNode(2);
-g.addNode(3);
-g.addEdge(2,3);
-g.removeNode(2);
-
